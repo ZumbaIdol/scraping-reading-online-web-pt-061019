4371429b9c1b3ed55a104b2cea6f288280709c28
David Foote <david.a.foote@gmail.com>
2019-07-22 17:21:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,8 @@ require 'open-uri'
 doc = Nokogiri::HTML(open("http://flatironschool.com/"))
 puts doc.css(".site-header__hero__headline").text.strip
 
-html = open("http://flatironschool.com/")
-doc = Nokogiri::HTML(html)
+courses = doc.css(".tout__label.heading.heading--level-4")
  
-doc.css(".tout__label.heading.heading--level-4")
+courses.each do |course|
+  puts course.text.strip
+end
