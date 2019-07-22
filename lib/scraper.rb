eca0a48668bd973c2ba1ded680e4f5b9b97a6340
David Foote <david.a.foote@gmail.com>
2019-07-22 17:03:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
-
-Nokogiri::HTML(html)
-
-doc = Nokogiri::HTML(html)
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc.css(".site-header__hero__headline")
 
 
